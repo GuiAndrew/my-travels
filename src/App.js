610feb46f6a890c913ebca92591ceed9b74a84f0
GuiAndrew <gfilipea001@gmail.com>
2020-01-11 15:50:32 +0000

    React 1 challenge my-travels

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Travel from './components/Travel';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Travel 
+        destination = "Azores"
+        country = "Portugal"
+        photo = "https://imagesvt.abreu.pt/abreu/share/2019-03/2019-03-20103809_a000263c-69b3-42c4-84ef-a24a06e6335b$$ee3b4c99-4882-441a-a1c4-b7b8cfdc9e43$$774f7b91-93ae-4831-9e5a-ae6f53110023$$BlogImageHeader$$pt$$1.jpg"
+        distance = "1000.00 KM"
+      />
+      <Travel 
+        destination = "Siberia"
+        country = "Russia"
+        photo = "https://image.businessinsider.com/5d409233100a247279644dd4?width=1900&format=jpeg&auto=webp"
+        distance = "5.000.00 KM"
+      />
     </div>
   );
 }
